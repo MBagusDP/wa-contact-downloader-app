e3146f89a9de9b9d6a85c99fcc670845091fb4fc
Mohammad Bagus Dwi Putra <bagus.mbdp@gmail.com>
2023-11-22 15:06:53 +0700

    Run flask as executable windows application

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app.routes import app
+import webview
+
+window = webview.create_window('MBagusDP Whatsapp Unsaved Contacts Downloader',app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    webview.start()
